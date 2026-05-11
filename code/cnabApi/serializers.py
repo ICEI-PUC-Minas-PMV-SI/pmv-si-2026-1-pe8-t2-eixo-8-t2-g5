7201d2ef0374009276701d59d356c54976d6727a
Jeziel Silva <60527278+jezil2210@users.noreply.github.com>
2026-05-10 21:58:47 -0300

    Delete code directory

--- code/cnabApi/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-# from cnab.models import Documentation
-
-#
-# class DocumentationSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Documentation
-#         fields = '__all__'
